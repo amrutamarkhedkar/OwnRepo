4a2293db6b3afb5272b2ca06e3f3ee3b57216ca8
amrutamarkhedkar <amruta.markhedkar@gmail.com>
2021-09-01 07:32:49 +0530

    Merging both branch changes

--- src/test/Class1.java
+++ src/test/Class1.java
@@ -3,8 +3,11 @@ package test;
 public class Class1 {
 
 	public static void main(String[] args) {
+
 		System.out.println("In branch A");
 		System.out.println("adding another line for Branch A");
-		
+		System.out.println("Now adding this line here from BranchB");
+		System.out.println("Branch B line 2");
+
 	}
 }
